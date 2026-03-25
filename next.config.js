2c1683b35b99c466497e2881bd357f287d70374b
dustin.nieves <dustiniztheman@gmail.com>
2026-03-25 17:04:15 -0400

    Fix next config for v14

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: ["@prisma/client"],
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
 };
 
 module.exports = nextConfig;
